cd7ef38003f95997e5856d89cb6869dbb68747c0
DeKingRey <tigermiguel456@gmail.com>
2026-02-16 09:55:53 +1300

    Enemy AI (beginning)

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private enum PathType
+    {
+        Loop,
+        ReverseWhenComplete
+    }
+
+    [Header("Pathfinding")]
+    [SerializeField] private Transform[] waypoints;
+    [SerializeField] private PathType pathType;
+
+    private NavMeshAgent agent;
+    private int currentWaypointIndex = 0;
+    private int direction = 1;
+
     void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private int GetNextWaypointIndex()
     {
-        
+        currentWaypointIndex += direction;
+
+        if (pathType == PathType.Loop) index %= waypoints.Length;
     }
 }
